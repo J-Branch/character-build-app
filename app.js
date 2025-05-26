cd91b17c7277b638cace8815565f6dfa32916b13
J-Branch <jamalbranch01@gmail.com>
2025-05-26 00:01:30 -0400

    added login and signup, added login route, register route and tested them

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // ================ Imports ====================
 const express = require('express');
 const path = require('path');
-
-
+const session = require('express-session');
+require('dotenv').config();
 // =============== App Setup ===================
 const app = express();
 
@@ -10,12 +10,24 @@ const app = express();
 // parse any incoming json
 app.use(express.json());
 
+// parse any from submitions
+app.use(express.urlencoded({ extended: true}))
+
 // static files that are from public/
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
 // =================== Routes =====================
 const buildRoutes = require('./routes/build-routes');
 app.use('/api/builds', buildRoutes);
+
+const accountRoutes = require('./routes/account-routes');
+app.use('/accounts', accountRoutes);
 
 // ================= Export App =================
 module.exports = app;
---
+++ routes/account-routes.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const path = require('path');
+const router = express.Router();
+
+// middleware to protect routes 
+function requireLogin(req, res, next) {
+    if(!req.session.userId) {
+        return res.status(401).json({ message: "not authorized" });
+    }
+    next();
+}
+
+// route for when a user wants to logout
+router.post('/logout', async (req, res) => {
+    req.session.destroy(err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).json({ message: "Logout failed" });
+        }
+
+        res.clearCookie('connect.sid');
+        res.redirect('/login');
+    });
+});
+
+// route for when the user makes an account
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+        const exists = await User.findOne({ username });
+        if(exists) {
+            return res.status(400).json({ message: "username already taken" });
+        }
+
+        
+        const hashedP = await bcrypt.hash(password, 10);
+
+        const newUser = new User({ username, password: hashedP});
+        await newUser.save();
+
+        // res.status(201).json({ message: "User registered successfully" });
+        res.redirect('/')
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error"} );
+    }
+});
+
+// route for when the user wants to login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if(!user) {
+            return res.status(400).json({ message: "Invalid username or password" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({ message: "Invalid username or password" });
+        }
+
+        req.session.userId = user._id;
+        // res.status(200).json({ message: "Login successful" });
+        res.sendFile(path.join(__dirname, '../views/profile.html'));
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: "server error" });
+    }
+});
+
+// route for if the user is currently logged in(EX. to look at my builds)
+router.get('/profile', requireLogin, (req, res) => {
+    res.status(200).json({ message: "welcome to your builds" });
+});
+
+module.exports = router;
